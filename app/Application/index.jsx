703a9497b34ac495175214d1c32d2921e5a9912f
Jason Brown <iam@browniefed.com>
2015-03-08 20:53:49 -0700

    Add email and info

--- app/Application/index.jsx
+++ app/Application/index.jsx
@@ -13,7 +13,10 @@ var Application = React.createClass({
             		This is a list of programs, tools, and apps built to extend Jama. This is currently an exclusive list, and requires review before being added to the list however in the future I hope this can change.
             	</p>
             	<p>	
-            		Apps on this list should all authenticate against JamaLand. Further instructions on how to develop for this will be posted later.
+            		Apps on this list should only authenticate against JamaLand. Further instructions on how to develop for this will be posted later.
+            	</p>
+            	<p>
+            		To request information, an app to be listed, or a feature request/prototype request email <a href="mailto:jbrown@jamasoftware.com">jbrown@jamasoftware.com</a>
             	</p>
             	<div className="content">
                 	<RouteHandler />
